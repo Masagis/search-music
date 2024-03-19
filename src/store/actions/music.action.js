4169770789d8849c1e3535b154dcbb6d958ed8e5
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-03-19 15:58:38 +0700

    fix(api): add prams load more

--- src/store/actions/music.action.js
+++ src/store/actions/music.action.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 import { actionTypes } from '../actionsTypes'
 
-import { REACT_APP_API_URL } from 'src/configs/keys'
+import { REACT_APP_API_URL } from 'src/config/keys'
 
 export function getSearchResult(params) {
   return (dispatch) => {
-    dispatch({ type: actionTypes.NGMUSIC_REQUEST })
+    dispatch({ type: actionTypes.NGMUSIC_REQUEST, payload: params })
     return axios
-      .get(`${REACT_APP_API_URL}search?term=${params}`)
+      .get(`${REACT_APP_API_URL}search`, { params })
       .then((response) => {
         dispatch({
           type: actionTypes.NGMUSIC_SUCCESS,
-          payload: response.data.results,
+          payload: response?.data?.results,
         })
         return response.data
       })
--- src/store/reducers/music.reducer.js
+++ src/store/reducers/music.reducer.js
@@ -3,6 +3,10 @@ import { actionTypes } from '../actionsTypes'
 const initState = {
   isLoading: false,
   musicsList: null,
+  params: {
+    term: '',
+    limit: 10,
+  },
 }
 
 const musics = (state = initState, action) => {
@@ -11,12 +15,13 @@ const musics = (state = initState, action) => {
       return {
         ...state,
         isLoading: true,
+        params: action.payload,
       }
     case actionTypes.NGMUSIC_SUCCESS:
       return {
         ...state,
         isLoading: false,
-        musicsList: action.payload || null,
+        musicsList: action.payload,
       }
     case actionTypes.NGMUSIC_FAILED:
       return {
