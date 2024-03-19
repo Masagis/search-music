2ec57070d018ff9ae5202bdfe16ec3b3d4d98fd7
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-03-19 15:14:37 +0700

    fix(env): add env gitignore

---
+++ src/config/keys.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-undef
+export const REACT_APP_API_URL = `${process.env.REACT_APP_URL_API}/`
--- src/store/actions/music.action.js
+++ src/store/actions/music.action.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { actionTypes } from '../actionsTypes'
 
+import { REACT_APP_API_URL } from 'src/configs/keys'
+
 export function getSearchResult(params) {
   return (dispatch) => {
     dispatch({ type: actionTypes.NGMUSIC_REQUEST })
     return axios
-      .get(`https://itunes.apple.com/search?term=${params}`)
+      .get(`${REACT_APP_API_URL}search?term=${params}`)
       .then((response) => {
         dispatch({
           type: actionTypes.NGMUSIC_SUCCESS,
